cabf6250f702b12ba40eb047cc5fa5a248b95e64
Mathias <mathias.benoit@student.vives.be>
2020-02-05 13:47:09 +0100

    oke pik hiers de modulo

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,9 +2,16 @@ package sample;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
     public TextField txtGetal1;
     public TextField txtGetal2;
     public Label lblResultaat;
+
+    public void BerekenModulo(MouseEvent mouseEvent) {
+        int iPate = Integer.parseInt(txtGetal1.getText()) % Integer.parseInt(txtGetal2.getText());
+        lblResultaat.setText("De rest is: " + iPate);
+
+    }
 }

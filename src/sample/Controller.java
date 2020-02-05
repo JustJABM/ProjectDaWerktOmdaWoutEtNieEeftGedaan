17d145b2d482e34b495e23f03fc733ea3032c045
KasperMartens <kasper.martens@student.vives.be>
2020-02-05 14:08:25 +0100

    Maal toegevoegd

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,9 +2,18 @@ package sample;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
     public TextField txtGetal1;
     public TextField txtGetal2;
     public Label lblResultaat;
+
+    public void click_btnMaal(MouseEvent mouseEvent) {
+        int getal1 = Integer.parseInt(txtGetal1.getText());
+        int getal2 = Integer.parseInt(txtGetal2.getText());
+
+        lblResultaat.setText(String.valueOf(getal1*getal2));
+    }
+
 }

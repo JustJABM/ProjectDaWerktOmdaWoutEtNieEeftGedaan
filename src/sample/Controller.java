4a56a783dd6537048e2b31ebc1ee9c3222360b8e
wouthostenss <wout.hostens@student.vives.be>
2020-02-05 14:06:10 +0100

    plus teken

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,9 +2,15 @@ package sample;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
     public TextField txtGetal1;
     public TextField txtGetal2;
     public Label lblResultaat;
+
+    public void plus_clicked(MouseEvent mouseEvent) {
+        int kaas = Integer.parseInt(txtGetal1.getText()) + Integer.parseInt(txtGetal2.getText());
+        lblResultaat.setText(String.valueOf(kaas));
+    }
 }

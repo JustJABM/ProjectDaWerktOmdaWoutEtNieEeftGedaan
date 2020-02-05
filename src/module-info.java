0daf567d8b761351d9f9046e3826d3e7f35211a8
Mathias <mathias.benoit@student.vives.be>
2020-02-05 12:25:58 +0100

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module ProjectDaWerktOmdaWoutEtNieEeftGedaan {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.desktop;
+
+    opens sample;
+
+
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public TextField txtGetal1;
+    public TextField txtGetal2;
+    public Label lblResultaat;
+}

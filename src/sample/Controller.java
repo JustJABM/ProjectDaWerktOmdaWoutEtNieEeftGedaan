d84764b5a5da75466d7fa54a18e709693d5179f5
kevdc <kevin.decoo@student.vives.be>
2020-02-05 13:32:44 +0100

    delen toegevoegd in controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -7,4 +9,13 @@ public class Controller {
     public TextField txtGetal1;
     public TextField txtGetal2;
     public Label lblResultaat;
+    public Button deelknop;
+
+    public void delen(ActionEvent actionEvent) {
+
+        double eerste = Double.parseDouble(txtGetal1.getText());
+        double tweede = Double.parseDouble(txtGetal2.getText());
+        double resultaat = eerste/tweede;
+        lblResultaat.setText(String.valueOf(resultaat));
+    }
 }

b111f187efe1ef950ae255eb21e2944a3654170c
Arne <arne.vandenborre@student.vives.be>
2020-02-05 13:39:41 +0100

    Min toegevoegd in controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,9 +2,15 @@ package sample;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
     public TextField txtGetal1;
     public TextField txtGetal2;
     public Label lblResultaat;
+
+    public void MouseClicked_Min(MouseEvent mouseEvent) {
+        int ham = Integer.parseInt(txtGetal1.getText()) - Integer.parseInt(txtGetal2.getText());
+        lblResultaat.setText(String.valueOf(ham));
+    }
 }

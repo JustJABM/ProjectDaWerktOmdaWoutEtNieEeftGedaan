7483b310f3b4967e490d68bc604c2be250157fc3
wouthostenss <wout.hostens@student.vives.be>
2020-02-05 14:11:07 +0100

    ee
    ee

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,12 +10,15 @@ public class Controller {
     public Label lblResultaat;
 
     public void plus_clicked(MouseEvent mouseEvent) {
-        int kaas = Integer.parseInt(txtGetal1.getText()) + Integer.parseInt(txtGetal2.getText());
-        lblResultaat.setText(String.valueOf(kaas));
+        try {
+            int kaas = Integer.parseInt(txtGetal1.getText()) + Integer.parseInt(txtGetal2.getText());
+            lblResultaat.setText(String.valueOf(kaas));
+        }
+        catch (Exception e)
+        {
+            lblResultaat.setText("probeer onpnieuw en vul getal in");
+        }
     }
 
-    public void fout()
-    {
-        
-    }
+
 }
